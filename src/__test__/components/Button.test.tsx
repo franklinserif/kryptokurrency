eb8a7a9814f1e895f3b997c5ea3ce687aa46d335
franklin rodriguez <franklinserif@gmail.com>
2022-03-25 16:34:21 -0400

    Fix problem with themeProvider and shallow enzyme test

--- src/__test__/components/Button.test.tsx
+++ src/__test__/components/Button.test.tsx
@@ -2,38 +2,39 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import ThemeProvideMock from '../../__mocks__/ThemeProviderMock';
-import { shallow } from 'enzyme';
+import { shallow, mount, MountRendererProps } from 'enzyme';
 import { create } from 'react-test-renderer';
 import Button from '../../components/Button/Button';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+import ThemeProviderMock from '../../__mocks__/ThemeProviderMock';
 
 describe('Button component', () => {
-  const button = shallow(
-    <ThemeProvideMock>
-      <Button variant="primary-button" handleClick={() => {}}>
-        Button
-      </Button>
-    </ThemeProvideMock>,
-  );
-  console.log(button);
-  it('Should render a Primery Button', () => {
-    expect(button.length).toEqual(1);
-  });
-
-  it('should have a Button text', () => {
-    expect(button.find('Button').text()).toEqual('<Button />');
-  });
-});
-
-describe('button Snapshot', () => {
-  it('test UI of Button component', () => {
-    const button = create(
-      <ThemeProvideMock>
-        <Button variant="primary-button" handleClick={() => {}}>
-          Button
-        </Button>
-      </ThemeProvideMock>,
+  const shallowWithTheme = (tree: React.ReactElement<any>, theme: DefaultTheme) => {
+    const WrappingThemeProvider: React.FC<{ children: React.ReactChild }> = (props) => (
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
     );
-    expect(button).toMatchSnapshot();
+
+    return mount(
+      tree,
+      { wrappingComponent: WrappingThemeProvider } as MountRendererProps, //Override typing because @types/enzyme doesn't have wrappingComponent yet https://github.com/DefinitelyTyped/DefinitelyTyped/pull/36667
+    );
+  };
+  /* 
+  const button = shallowWithTheme(
+    <Button variant="primary-button" handleClick={() => {}}>
+      Text
+    </Button>,
+    mainTheme,
+  ); */
+
+  it('sould render correctly in debug mode', () => {
+    const button = create(
+      <ThemeProviderMock>
+        <Button variant="primary-button" handleClick={() => {}}>
+          Text
+        </Button>
+      </ThemeProviderMock>,
+    );
+    expect(button.toJSON()).toMatchSnapshot();
   });
 });
